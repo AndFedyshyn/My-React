0b2c3929291e01d9a8a90917b5b483f514b5e125
AndFedyshyn <andfedyshyn@gmail.com>
2021-01-14 23:51:14 +0200

    hw7 (my ArmShop)

--- myreact-hw7/src/App.js
+++ myreact-hw7/src/App.js
@@ -2,14 +2,15 @@ import React from 'react';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import {Container} from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
     return (
         <>
             <Header/>
-            <main>
+            <main className={'py-3'}>
                 <Container>
-                <h1>Welcome to ArmShop</h1>
+                    <HomeScreen/>
                 </Container>
             </main>
             <Footer/>
