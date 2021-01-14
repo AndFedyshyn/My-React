9497db98307aab92761ea40bf0d778a44e349088
AndFedyshyn <andfedyshyn@gmail.com>
2021-01-15 00:30:38 +0200

    hw7 (my ArmShop)

--- myreact-hw7/src/components/Product.js
+++ myreact-hw7/src/components/Product.js
@@ -5,7 +5,7 @@ const Product = ({product}) => {
     return (
         <Card className='my-3 p-3 rounded'>
             <a href={`/product/${product._id}`}>
-                <Card.Img scr={product.image} variant='top'/>
+                <Card.Img src={product.image} variant='top' />
             </a>
             <Card.Body>
                 <a href={`/product/${product._id}`}>
